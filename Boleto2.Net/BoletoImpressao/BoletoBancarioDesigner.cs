d3d87422323db3292e8b7015c9bc6be5d5d559ec
Olavo Rocha <olavo@exodus.eti.br>
2019-07-07 22:08:36 -0300

    Versão Inicial do Boleto.Net Core Projeto Versão netstandard 2.2 Nova estrutura de arquivos de resource. Permite rodar em Windows/Linux sem System.Web Ajustes na Formatação de CEP Ajustes na Carteira Banco do Brasil Cart. 17 Retirada do projeto principal de referências a biblioteca PDF (Para uso do PDF, utilizar a extensão Boleto.Net.Pdf) Testes passando a excessão do SICREDI .

--- Boleto2.Net/BoletoImpressao/BoletoBancarioDesigner.cs
+++ Boleto2.Net/BoletoImpressao/BoletoBancarioDesigner.cs
@@ -3,32 +3,32 @@ using System.ComponentModel;
 
 namespace Boleto2Net
 {
-    internal class BoletoBancarioDesigner : System.Web.UI.Design.ControlDesigner
-    {
-        public override void Initialize(IComponent component)
-        {
-            base.Initialize(component);
-        }
+    //internal class BoletoBancarioDesigner : System.Web.UI.Design.ControlDesigner
+    //{
+    //    public override void Initialize(IComponent component)
+    //    {
+    //        base.Initialize(component);
+    //    }
 
-        // Make this control resizeable on the design surface
-        public override bool AllowResize
-        {
-            get
-            {
-                return true;
-            }
-        }
+    //    // Make this control resizeable on the design surface
+    //    public override bool AllowResize
+    //    {
+    //        get
+    //        {
+    //            return true;
+    //        }
+    //    }
 
-        public override string GetDesignTimeHtml()
-        {
-            return CreatePlaceHolderDesignTimeHtml("Boleto.Net.BoletoBancario");
-        }
+    //    public override string GetDesignTimeHtml()
+    //    {
+    //        return CreatePlaceHolderDesignTimeHtml("Boleto.Net.BoletoBancario");
+    //    }
 
 
-        protected override string GetErrorDesignTimeHtml(Exception e)
-        {
-            string pattern = "Boleto.Net while creating design time HTML:<br/>{0}";
-            return String.Format(pattern, e.Message);
-        }
-    }
+    //    protected override string GetErrorDesignTimeHtml(Exception e)
+    //    {
+    //        string pattern = "Boleto.Net while creating design time HTML:<br/>{0}";
+    //        return String.Format(pattern, e.Message);
+    //    }
+    //}
 }
